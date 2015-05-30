44aec4a72a8ec78f252eb4d0c6a5c3d99df837f5
Michael Telatynski <7t3chguy@gmail.com>
2015-05-30 22:28:57 +0100

    test

--- SpiffyTitles/plugin.py
+++ SpiffyTitles/plugin.py
@@ -47,7 +47,7 @@ def getPlugins():
     print(possibleplugins)
     for i in possibleplugins:
         location = os.path.join(PluginFolder, i)
-        if not os.path.isdir(location) or not MainModule + ".py" in os.listdir(location):
+        if os.path.isdir(location) or not MainModule + ".py" in os.listdir(location):
             continue
         info = imp.find_module(MainModule, [location])
         plugins.append({"name": i, "info": info})
