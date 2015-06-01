4fc598853a62d6b7dd1ff9df3581b24e01115c4a
Michael Telatynski <7t3chguy@gmail.com>
2015-06-01 15:08:16 +0100

    test

--- SpiffyTitles/handlers/__init__.py
+++ SpiffyTitles/handlers/__init__.py
@@ -1,10 +1,10 @@
 import os
 import glob
 from . import imdb
-from . import *
 
 modules = glob.glob(os.path.dirname(__file__)+"/*.py")
 __all__ = [ os.path.basename(f)[:-3] for f in modules]
 
 class HandlerMain:
+	from . import *
 	__all__ = [ os.path.basename(f)[:-3] for f in glob.glob(os.path.dirname(__file__)+"/*.py")]
