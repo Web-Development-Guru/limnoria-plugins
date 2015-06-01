9ef29bb43f3d489e699b8cfe3e000ad1fc79baac
Michael Telatynski <7t3chguy@gmail.com>
2015-06-01 18:51:27 +0100

    test

--- SpiffyTitles/plugin.py
+++ SpiffyTitles/plugin.py
@@ -75,7 +75,7 @@ class SpiffyTitles(callbacks.Plugin):
         print(handlers.__all__)
         for name in handlers.__all__:
             print("Importing %s" % name)
-            mod = __import__('handlers.' + name)
+            mod = __import__('handlers.' + name, globals=globals())
             print(dir(mod))
         #for name in handlers.__all__:
             #importlib.import_module('.handlers.' + name, self)
