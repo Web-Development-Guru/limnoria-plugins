720e3a5056215c0e1e638f1099be53cfb35f2fe5
Michael Telatynski <7t3chguy@gmail.com>
2015-06-01 17:11:03 +0100

    test

--- SpiffyTitles/handlers/__init__.py
+++ SpiffyTitles/handlers/__init__.py
@@ -2,4 +2,4 @@ from os import path
 import glob
 
 modules = glob.glob(path.dirname(__file__)+"/*.py")
-__all__ = [ path.basename(f)[:-3] for f in modules if not name.startswith('_')]
+__all__ = [ path.basename(f)[:-3] for f in modules if not f.startswith('_')]
