9597199ebf6a8204c4b859775820b1a1dd10de75
Michael Telatynski <7t3chguy@gmail.com>
2015-06-01 15:18:38 +0100

    test

--- SpiffyTitles/handlers/__init__.py
+++ SpiffyTitles/handlers/__init__.py
@@ -8,5 +8,9 @@ handles = {}
 for p in __all__:
 	handles[p] = __import__(p)
 
+print(dir(handles))
+
 class HandlerMain:
 	__all__ = [ os.path.basename(f)[:-3] for f in glob.glob(os.path.dirname(__file__)+"/*.py")]
+	for p in __all__:
+		handles[p] = __import__(p)
